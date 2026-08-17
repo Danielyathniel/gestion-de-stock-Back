1c20e5353072b2cd937615bf0ad4bae04321cf64
tossougbo-mariel <marielhinson22@gmail.com>
2026-08-17 16:44:17 +0100

    impression

--- app/Http/Controllers/Api/CategorieController.php
+++ app/Http/Controllers/Api/CategorieController.php
@@ -23,7 +23,7 @@ class CategorieController extends Controller
     public function store(Request $request): JsonResponse
     {
         $validated = $request->validate([
-            'nom' => 'required|string|max:150|unique:categories,nom|regex:/^(?=.*\pL)[\pL\d\s\'-]+$/u',
+            'nom' => 'required|string|min:3|max:150|unique:categories,nom|regex:/^(?=.*\pL)[\pL\d\s\'-]+$/u',
         ]);
 
         $categorie = Categorie::create($validated);
@@ -39,7 +39,7 @@ class CategorieController extends Controller
     public function update(Request $request, Categorie $categorie): JsonResponse
     {
         $validated = $request->validate([
-            'nom' => 'required|string|max:150|unique:categories,nom,' . $categorie->id,
+            'nom' => 'required|string|min:3|max:150|unique:categories,nom,' . $categorie->id,
         ]);
 
         $categorie->update($validated);
