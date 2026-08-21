d374e767efb3ef3da6efa9958cc28ae2f84f542e
DanielYathniel <allianceodah@gmail.com>
2026-08-21 03:32:29 -0800

    Fix start.sh - LF line endings + debug logs

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,11 @@
-﻿#!/bin/bash
+#!/bin/bash
+echo "Starting deploy script..."
 php artisan config:cache
+echo "Config cached"
 php artisan route:cache
+echo "Routes cached"
 php artisan migrate --force
-
+echo "Migrations done"
 service nginx start
+echo "Nginx started"
 php-fpm
